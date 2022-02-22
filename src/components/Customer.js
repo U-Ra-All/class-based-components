e1e3224388381b4f1dcba38c0adb665165a970e5
YouRa Allakhverdov <youraall@apples-MacBook-Pro.local>
2022-02-22 15:35:10 +0200

    06-Life-cycle Методы

--- src/components/Customer.js
+++ src/components/Customer.js
@@ -2,6 +2,10 @@ import { Component } from "react";
 import styles from "./Customer.module.css";
 
 class Customer extends Component {
+  componentWillUnmount() {
+    console.log("Заказчик будет удалён!");
+  }
+
   render() {
     return <li className={styles.customer}>{this.props.name}</li>;
   }
